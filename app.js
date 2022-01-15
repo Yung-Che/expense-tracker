fb1024df7879512732361a2328ad9429ab811096
DAI <senseye935@gmail.com>
2022-01-15 16:06:17 +0800

    refactor: modift mongoose config

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 // 載入 express
 const express = require('express')
-const mongoose = require('mongoose')
+
 
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-
-const routes = require('./routes')
 const router = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 
-// 設定連線mongodb
-mongoose.connect('mongodb://localhost/expense-tracker')
 
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+
 
 // hbs 設定
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,19 +1,12 @@
-const mongoose = require('mongoose')
 const Record = require('../record')
 
 const records = require('./record.json').results
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/expense-tracker')
-
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 // 連線成功
 db.once('open', () => {
   Record.create(records)
-  console.log('mongodb connected!')
+  console.log('done!')
 })
 
