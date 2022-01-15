095910eb40572dfbd27f153cdbb862f1c39b4341
DAI <senseye935@gmail.com>
2022-01-15 10:45:49 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 // 載入 express
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+// 設定連線mongodb
+mongoose.connect('mongodb://localhost/expense-tracker')
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
 
 // 設定路由
 app.get('/', (req, res) => {
