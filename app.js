fd01476824dfc45f10ee3cc127cbf36cc4967a95
DAI <senseye935@gmail.com>
2022-01-16 06:55:04 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 // 載入 express
 const express = require('express')
 const session = require('express-session')
+const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
+const flash = require('connect-flash')
 const router = require('./routes')
 const usePassport = require('./config/passport')
 require('./config/mongoose')
 
-const app = express()
+
 
 
 // hbs 設定
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
@@ -23,11 +25,19 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 usePassport(app)
+
+
+app.use(flash())
+
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error_msg = req.flash('error')
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,35 +3,56 @@ const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
 
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
   User.findOne({ email }).then(user => {
     if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       console.log('User already exists.')
-      res.render('register', {
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
     }
+    return User.create({
+      name,
+      email,
+      password
+    })
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
   })
 })
 
